ae4ccce2cd6b1cf467d3659438713331889fe778
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-19 17:24:27 -0600

    implement my_map, my_map_proc and my_map_block methods

--- enumerables.rb
+++ enumerables.rb
@@ -69,9 +69,29 @@ module Enumerables
   end
 
   def my_map
-    item_select = []
-    my_each { |i| item_select << i if yield(i) }
-    item_select
+    items = []
+    my_each { |i| items << i if yield(i) }
+    items
+  end
+
+  def my_map_proc(&proc_block)
+    items = []
+    my_each { |i| items << proc_block.call(i) }
+    items
+  end
+
+  def my_map_block(param = nil)
+    i = 0
+    items = []
+    while i < size
+      if param.nil? && block_given?
+        items << yield(self[i])
+      elsif !param.nil && block_given?
+        items << param.call(self[i])
+      end
+      i += 1
+    end
+    items
   end
 
   def my_inject
