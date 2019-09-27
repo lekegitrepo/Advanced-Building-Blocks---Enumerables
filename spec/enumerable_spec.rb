48d1ee572c6c8674b118d43fd0b862315741ce53
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-27 17:44:23 -0600

    add tests structures

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,41 @@
+require './enumerable'
+
+RSpec.describe Enumerable do
+  let arr = [4, 5, 8, 2, 1, 9]
+
+  describe 'my_each' do
+    it 'return' do
+      expect().to eql()
+    end
+  end
+
+  describe 'my_each_with_index' do
+    it 'return' do
+      expect().to eql()
+    end
+  end
+
+  describe 'my_count' do
+    it 'return' do
+      expect().to eql()
+    end
+  end
+
+  describe 'my_map' do
+    it 'return' do
+      expect().to eql()
+    end
+  end
+
+  describe 'my_none' do
+    it 'return' do
+      expect().to eql()
+    end
+  end
+
+  describe 'my_select' do
+    it 'return' do
+      expect().to eql()
+    end
+  end
+end
